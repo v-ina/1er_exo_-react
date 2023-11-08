ee7d548a02c681f834b3762bf19f667f3a29658d
yeonsookang <yeonsoo.kang@lapiscine.pro>
2023-11-08 12:02:40 +0100

    using arrays

--- src/Products.js
+++ src/Products.js
@@ -1,21 +1,20 @@
 function Products(){
+
+    const productsFromDb = [
+        "Mocassin à glands" , "Charenaises" , "Talons aiguilles"
+    ] //pour boucler, on plutot utilise .map() au JSX
+
     return(
         <div className="product">
-            <article>
-                <h3>Mon product1</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet magna at sem luctus blandit ut vitae felis. </p>
-                <button>en savoir +</button>
-            </article>
-            <article>
-                <h3>Mon product2</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet magna at sem luctus blandit ut vitae felis. </p>
-                <button>en savoir +</button>
-            </article>
-            <article>
-                <h3>Mon product3</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet magna at sem luctus blandit ut vitae felis. </p>
-                <button>en savoir +</button>
-            </article>
+            {productsFromDb.map((product)=>{
+                return (
+                    <article>
+                        <h3>{product}</h3>
+                        <p>Lorem ipsum dolor sit amet, <strong>{product}</strong> consectetur adipiscing elit. Maecenas sit amet magna at sem luctus blandit ut vitae felis.</p>
+                        <button>en savoir {product} +</button>
+                    </article>
+                )
+            })}
         </div>
     )
 }
