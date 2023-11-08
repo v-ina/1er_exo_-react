e31700ac4d6ab65bf0984a80c7dcb35d28967bad
yeonsookang <yeonsoo.kang@lapiscine.pro>
2023-11-08 10:39:24 +0100

    exercice panier

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,8 @@ import logo from "./img/google.svg"
 
 function Header (){
 
-    const isUserLoggedIN = true
+    const isUserLoggedIn = true
+    const itemsInCart = 2
 
     return (
         <header>
@@ -12,11 +13,13 @@ function Header (){
                     <li><a href="">ACCEUIL</a></li>
                     <li><a href="">ABOUT US</a></li>
                     <li>
-                        {isUserLoggedIN ? (
+                        {isUserLoggedIn ? (
                             <a href="">MY PAGE</a>
                             ):( <button>CONNECTEZ-VOUS</button>
-                        )}
-                            
+                        )}  
+                    </li>
+                    <li>
+                        {itemsInCart > 0 ? <p>Cart : {itemsInCart}</p> : <p>Pas d'item au panier</p>}
                     </li>
                 </ul>
             </nav>
